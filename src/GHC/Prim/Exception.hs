ceb3666a49c3adb125bdfd707d1a54c429b17574
Kyle Cranmer <hobbitprog@breeprogramming.org>
2023-09-08 10:43:18 -0500

    Updated for GHC 9.2.5.

---
+++ src/GHC/Prim/Exception.hs
@@ -0,0 +1,4 @@
+module GHC.Prim.Exception (module Exports) where
+
+import GHC.Types
+import "ghc-prim" GHC.Prim.Exception as Exports
---
+++ src/GHC/Prim/Panic.hs
@@ -0,0 +1,4 @@
+module GHC.Prim.Panic (module Exports) where
+
+import GHC.Types
+import "ghc-prim" GHC.Prim.Panic as Exports
